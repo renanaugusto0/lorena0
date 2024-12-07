499f2d3fade3b6279bd1c9ada533a59ea007b1b7
Renan Augusto Veloso de Medeiros <renanaugustovelosodemedeiros@gmail.com>
2024-12-07 11:43:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,5 +38,29 @@ function updateTimer() {
   document.getElementById('timer').innerHTML =
     `${months} meses, ${days} dias, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
 }
+particlesJS("particles-js", {
+  particles: {
+      number: { value: 150, density: { enable: true, value_area: 800 } },
+      color: { value: "#ffdd57" },
+      shape: {
+          type: "circle",
+          stroke: { width: 0, color: "#000" },
+      },
+      opacity: { value: 0.7, random: true },
+      size: { value: 4, random: true },
+      line_linked: { enable: false },
+      move: { enable: true, speed: 2, direction: "none", random: true, out_mode: "out" },
+  },
+  interactivity: {
+      detect_on: "canvas",
+      events: {
+          onhover: { enable: true, mode: "grab" },
+          onclick: { enable: true, mode: "push" },
+      },
+      modes: { grab: { distance: 140 }, push: { particles_nb: 4 } },
+  },
+  retina_detect: true,
+});
+
 
 setInterval(updateTimer, 1000);
